57bc364a0b83fa89e073ecf4e4736fe131b808cb
Jonathan Sexton <jsexton@gibsonplumbing.com>
2019-10-04 07:01:57 -0500

    added placeholder info for recipe, and structure for recipe in html

--- src/app/components/recipes/recipes-list/recipes-list.component.ts
+++ src/app/components/recipes/recipes-list/recipes-list.component.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipes.model';
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-   recipes = [];
+  recipes: Recipe[] = [new Recipe('Cookies From Home', 'These cookies are bomb.com', 'https://i2.wp.com/www.livewellbakeoften.com/wp-content/uploads/2018/08/MM-Chocolate-Chip-Cookies-6.jpg?resize=500%2C500&ssl=1')];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
