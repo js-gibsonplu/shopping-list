a536975c69e281344867b40998de6b3ae189bb43
Jonathan Sexton <jsexton@gibsonplumbing.com>
2019-10-04 07:04:49 -0500

    added imgAlt to the recipes model

--- src/app/components/recipes/recipes.model.ts
+++ src/app/components/recipes/recipes.model.ts
@@ -2,10 +2,12 @@ export class Recipe {
     public name: string;
     public description: string;
     public imgPath: string;
+    public imgAlt: string;
 
-    constructor(name: string, desc: string, imgpath: string) {
+    constructor(name: string, desc: string, imgpath: string, imgalt: string) {
         this.name = name;
         this.description = desc;
         this.imgPath = imgpath;
+        this.imgPath = imgalt;
     }
 }
