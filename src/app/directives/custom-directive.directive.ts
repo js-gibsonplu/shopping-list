80fcdec97d708468c37f4fb42132fd8be27f8823
Jonathan Sexton <jsexton@gibsonplumbing.com>
2019-12-09 07:44:35 -0600

    changed custom directive & added a test paragraph

--- src/app/directives/custom-directive.directive.ts
+++ src/app/directives/custom-directive.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appCustomDirective]'
 })
-export class CustomDirectiveDirective {
+export class CustomDirectiveDirective implements OnInit {
 
-  constructor() { }
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'orange');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'orange');
+  }
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+  }
 
 }
