f6cdbf4e1e10f6f5205a6c25728908d4681656d8
Jonathan Sexton <jsexton@gibsonplumbing.com>
2019-12-19 07:29:09 -0600

    testing ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component, Input, Output } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  myVal=`Default`;
   title = 'shopping-list';
   @Output() recipesPageActive: boolean;
   displayFeature = 'recipes';
