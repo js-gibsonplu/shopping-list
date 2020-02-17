0ed8ee82f4e9e71fa4950e59d4c0b21f0ed80d20
Jonathan Sexton <jsexton@gibsonplumbing.com>
2020-02-17 06:55:38 -0600

    moved recipes data to the service

--- src/app/components/recipes/recipes-list/recipes-list.component.ts
+++ src/app/components/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipes.model';
+import { RecipesService, RecipeService } from '../../../services/recipe.service';
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
@@ -7,26 +9,13 @@ import { Recipe } from '../recipes.model';
 })
 export class RecipesListComponent implements OnInit {
   @Output() recipeThatWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'Cookies From Home',
-      'These cookies are bomb.com',
-      'https://i2.wp.com/www.livewellbakeoften.com/wp-content/uploads/2018/08/MM-Chocolate-Chip-Cookies-6.jpg?resize=500%2C500&ssl=1',
-      'some chocolate chip cookies with M&Ms',
-      'homeCookies'
-    ),
-    new Recipe(
-      'Cookies From Far Away',
-      'These cookies are great',
-      'https://i2.wp.com/www.livewellbakeoften.com/wp-content/uploads/2018/08/MM-Chocolate-Chip-Cookies-6.jpg?resize=500%2C500&ssl=1',
-      'some chocolate chip cookies with M&Ms',
-      'farAwayCookies'
-    )
-  ];
+  recipes: Recipe[];
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onSelectedRecipe(event, recipe: Recipe) {
     console.warn(`HELLO FROM RECIPE LIST ${event}`);
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from '../components/recipes/recipes.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Cookies From Home',
+      'These cookies are bomb.com',
+      'https://i2.wp.com/www.livewellbakeoften.com/wp-content/uploads/2018/08/MM-Chocolate-Chip-Cookies-6.jpg?resize=500%2C500&ssl=1',
+      'some chocolate chip cookies with M&Ms',
+      'homeCookies'
+    ),
+    new Recipe(
+      'Cookies From Far Away',
+      'These cookies are great',
+      'https://i2.wp.com/www.livewellbakeoften.com/wp-content/uploads/2018/08/MM-Chocolate-Chip-Cookies-6.jpg?resize=500%2C500&ssl=1',
+      'some chocolate chip cookies with M&Ms',
+      'farAwayCookies'
+    )
+  ];
+
+  getRecipes() {
+    // * This returns a copy of the recipes array
+    return this.recipes.slice();
+  }
 
   constructor() { }
 }
