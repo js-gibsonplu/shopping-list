76c4fe2b7ec669fff4528ffb26ee11c0d7c29a2b
Jonathan Sexton <jsexton@gibsonplumbing.com>
2020-02-24 07:39:34 -0600

    updated recipes model to include ingredients

--- src/app/components/recipes/recipes.model.ts
+++ src/app/components/recipes/recipes.model.ts
@@ -1,15 +1,18 @@
+import { Ingredient } from '../../shared-data/ingredient.model';
 export class Recipe {
     public name: string;
     public description: string;
     public imgPath: string;
     public imgAlt: string;
     public title: string;
+    public ingredients: Ingredient[];
 
-    constructor(name: string, desc: string, imgpath: string, imgalt: string, title: string) {
+    constructor(name: string, desc: string, imgpath: string, imgalt: string, title: string, ingredients: Ingredient[]) {
         this.name = name;
         this.description = desc;
         this.imgPath = imgpath;
         this.imgAlt = imgalt;
         this.title = title;
+        this.ingredients = ingredients;
     }
 }
